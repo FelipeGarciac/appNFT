7fa5a56e6785e60e7bc6e81c15d9832d42f0e7c4
felipe garcia <garcia.felipe180180@gmail.com>
2023-11-05 20:32:01 -0300

    fazendo a interna

---
+++ AppBackFront/Feature/Home/HomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  Home VC.swift
+//  AppBackFront
+//
+//  Created by Felipe Garcia on 05/11/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+
+
+}
